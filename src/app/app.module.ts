b9f1a216353cf8e208baa150940e2d06f0970a00
tryyourskill <ssahoo@tryyourskill.com>
2017-10-01 10:50:51 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { User } from './user';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login.component';
+import {HomeComponent} from './home.component';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AuthGuard} from './auth-guard.service';
+import {BackEndService} from './backend-service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthGuard,BackEndService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
