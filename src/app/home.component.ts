c74c0ebd7fa06d46e46f0a82e82924b917e646a0
tryyourskill <ssahoo@tryyourskill.com>
2017-10-01 10:59:47 +0530

    updated

---
+++ src/app/home.component.ts
@@ -0,0 +1,41 @@
+import { Component,Input,OnInit } from '@angular/core';
+import { User } from './user';
+import { Headers, Http,RequestOptions } from '@angular/http';
+import {Router, ActivatedRoute} from '@angular/router';
+import {BackEndService} from './backend-service';
+
+
+@Component({
+  selector: 'home-app',
+  template: `
+  <div *ngIf="user">
+    <h2>{{loggedInUser}}</h2>
+    <div>
+    <button (click)="logout()">Logout</button>
+    <button (click)="getUpdatedUser()">Get Updated User</button>
+    </div>
+  </div>
+  `
+})
+
+export class HomeComponent  {
+  @Input('loggedInUser') loggedInUser: string
+  constructor(private http: Http, private router: Router,private backEndService :BackEndService) {
+  }
+  getUrl = "http://localhost:9090/getUpdatedUser";   
+  logout(): void {
+    localStorage.removeItem('currentUser');
+    this.router.navigateByUrl('/login');
+    
+  }
+  user = new User(JSON.parse(localStorage.getItem('currentUser'))
+                          .userName,"");
+  getUpdatedUser(): void{
+    this.backEndService.getUpdatedUser(this.user).subscribe(response => {
+      this.loggedInUser = response.username;
+    }, (error) => {
+      console.log('error in', error);
+    });
+  }
+}
+
